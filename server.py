595fbe531a54b19640b932c636263053d950369e
Rick McGeer <rick@mcgeer.com>
2025-02-20 09:40:36 -0800

    Update server.py

--- server.py
+++ server.py
@@ -120,7 +120,7 @@ def list_user_dashboards(user):
     return _list_blobs('user-galyleo-dashboards', prefix)
 
 def _make_url(blob_name):
-    return f'https://galyleo.app/{blob_name}'
+    return jsonify({"dashboard": f'https://galyleo.app/{blob_name}'})
 
 def _valid_secret(secret):
     # Return True iff secret is not none and it matches the secret
